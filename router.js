1a97fcf57e9436c91d866fc10801edada233158f
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-12-25 23:52:26 +0900

    new add pug, controller, routes, router

--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router(); // 이것을 내보내는것이다.
-
-userRouter.get("/", (req, res) => res.send("user index"));
-userRouter.get("/edit", (req, res) => res.send("user edit"));
-userRouter.get("/password", (req, res) => res.send("user password"));
