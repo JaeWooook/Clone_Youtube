6dae1094994366c98a5183e48581bc6d9310b84c
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-12-27 09:16:57 +0900

    add video, fakedb, helmet error update

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   const {
@@ -10,14 +14,14 @@ export const search = (req, res) => {
 export const upload = (req, res) =>
   res.render("Upload", { pageTitle: "Upload" });
 
-export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
+// export const videos = (req, res) =>
+//   res.render("Videos", { pageTitle: "Videos" });
 
 export const videoDetail = (req, res) =>
-  res.render("Video Detail", { pageTitle: "Video Detail" });
+  res.render("VideoDetail", { pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) =>
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("EditVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("Delete Video", { pageTitle: "Delete Video" });
+  res.render("DeleteVideo", { pageTitle: "Delete Video" });
