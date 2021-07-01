bf75e6924468aa2764aa81ca306ab99cf518bd52
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-07-02 04:21:33 +0900

    mongoDB connected

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
-import routes from "./routes";
+// import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
 app.use(helmet());
--- src/db.js
+++ src/db.js
@@ -1,54 +1,13 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 121212,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 55555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 11111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/YourTube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log(" ✅ Connected to DB");
+const handleError = () => console.log(" ❌ DB Error", error);
+db.on("error", handleError);
+db.once("open", handleOpen);
