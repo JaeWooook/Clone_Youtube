04ef88afae8eb9db821271c6f752238a4edd3803
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-07-05 01:07:38 +0900

    video detail page update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,10 +5,10 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching: ` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = (req, res) => {
@@ -32,16 +32,13 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      createdAt: Date.now(),
       hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta: {
-        views: 0,
-        rating: 0,
-      },
     });
+    return res.redirect("/");
   } catch (error) {
-    console.log("Error : ", error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
   }
-
-  return res.redirect("/");
 };
