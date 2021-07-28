17562f349c972e8afcd9699a192c837db72ecc01
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-07-29 00:07:54 +0900

    add github login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 import routes from "../routes";
 
 const userRouter = express.Router();
@@ -11,6 +18,8 @@ const userRouter = express.Router();
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
