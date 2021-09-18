4860f5dbf782d14b6b7865340c92d6f9c67850e1
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-09-18 23:43:35 +0900

    update Mute & UnMute, Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,10 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue; //default volume을 0.5로 지정
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -10,12 +13,33 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue; //음소거를 풀면 기존의 볼륨으로 돌아가도록한다.
+};
 const handlePlay = () => (playBtn.innerText = "Pause");
 const handlePause = () => (playBtn.innerText = "Play");
 
+const handleVolumeChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("play", handlePlay);
-video.addEventListener("pause", handlePause);
+volumeRange.addEventListener("input", handleVolumeChange); //변경되는 e값을 출력한다. 마우스를 내려놓을때 값을 감지한다. change
+//실시간으로 감지되는것은 input이다.
