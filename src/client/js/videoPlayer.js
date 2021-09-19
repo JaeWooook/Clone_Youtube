00a4edb13efb457c2b785a6495eb539e1702af66
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-09-19 16:23:04 +0900

    update video controler

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,9 +3,14 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeLine = document.getElementById("timeLine");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
 let volumeValue = 0.5;
-video.volume = volumeValue; //default volume을 0.5로 지정
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -22,7 +27,7 @@ const handleMute = (e) => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : volumeValue; //음소거를 풀면 기존의 볼륨으로 돌아가도록한다.
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 const handlePlay = () => (playBtn.innerText = "Pause");
 const handlePause = () => (playBtn.innerText = "Play");
@@ -39,7 +44,44 @@ const handleVolumeChange = (e) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substr(11, 8); //시간초를 이용해서 포맷을 만들어준다.
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeLine.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeLine.value = Math.floor(video.currentTime);
+};
+
+const handleTimeLineChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  video.currentTime = value; //video.currentTime은 getter or setter를 동시에 해줄 수 있기 때문에 변경되는 value값을 그대로 넣어준다.
+};
+
+const hanldeFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    //fullscreen이 아니면 null이다.
+    //exit full screen은 document에서 불러진다.
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen(); //video api를 이용해서 fullscreen으로 만들어준다. requestfullscreen은 element에서 불러진다.
+    //videoContainer를 풀스크린으로해주면 컨트롤버튼까지 풀스크린으로된다.
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-volumeRange.addEventListener("input", handleVolumeChange); //변경되는 e값을 출력한다. 마우스를 내려놓을때 값을 감지한다. change
-//실시간으로 감지되는것은 input이다.
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate); //비디오의 시간이 변할때마다 업데이트된다.
+timeLine.addEventListener("input", handleTimeLineChange); //input의 변화하는 value값을 확인한다.
+fullScreenBtn.addEventListener("click", hanldeFullScreen);
