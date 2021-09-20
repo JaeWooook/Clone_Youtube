bb9343829797c51e223d4d95591a7f7d80c57e07
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-09-20 17:32:16 +0900

    add webm -> transform mp4

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,10 +7,30 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({
+    log: true,
+    corePath: "/static/ffmpeg-core.js",
+  });
+  await ffmpeg.load(); //await을 하는 이유는 사용자가 소프트웨어를 사용할 것이기 때문이다.
+
+  //ffmpeg파일을 만든다.
+  ffmpeg.FS("writeFile", "Myrecording.webm", await fetchFile(videoFile)); //writeFile은 ffmpeg의 가상 세계에 파일을 생성해준다.
+  //타입 , 파일명, 바이너리data를 넣어준다
+
+  await ffmpeg.run("-i", "Myrecording.webm", "-r", "60", "output.mp4"); //ffmpeg실행
+  //webm이 mp4로 변환되는 것이다. -r 60은  초당 60프레임으로 인코딩 해주라는 명령어이다. 더 빠른 영상을 인코딩을 가능하게해준다.
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4"); //파일을 읽는다. output.mp4라는 파일을
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" }); //type이 mp4라는것을 알려줘야한다.
+
+  const mp4Url = URL.createObjectURL(mp4Blob); //이것을 통해서 url을 생성해준다,
+  console.log(mp4File);
+  console.log(mp4File.buffer);
   const a = document.createElement("a"); //링크를생성하고
-  a.href = videoFile; //videoFile로 갈 수 있는 url과 연결한다.
-  a.download = "MyRecording.webm"; //사용자로 부터 url을 다운로드 하도록 해준다.
+  a.href = mp4Url; //videoFile로 갈 수 있는 url과 연결한다.
+  a.download = "MyRecording.mp4"; //사용자로 부터 url을 다운로드 하도록 해준다.
   document.body.appendChild(a);
   a.click();
 };
